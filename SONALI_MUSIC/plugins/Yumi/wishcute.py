437464a4523926c53cbab45b021cfe9dfdd80244
ᴧʟᴘʜᴧ ʙᴧʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2025-05-17 23:45:29 +0530

    ᴘᴜʀᴠɪ ʙᴏᴛs !!

--- SONALI_MUSIC/plugins/Yumi/wishcute.py
+++ SONALI_MUSIC/plugins/Yumi/wishcute.py
@@ -4,7 +4,7 @@ import random
 import requests
 from SONALI_MUSIC import app 
 
-SUPPORT_CHAT = "THE_INCRICIBLE"
+SUPPORT_CHAT = "PURVI_UPDATES"
 
 @app.on_message(filters.command("wish"))
 async def wish(_, m):
