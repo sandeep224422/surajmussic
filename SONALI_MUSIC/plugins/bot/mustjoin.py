b636fcf17cb2354af7d7d63a9d9aeef78a2aaf8f
Ankit kumar <ankitgupta2ndsp@gmail.com>
2025-08-29 15:37:38 +0530

    Update mustjoin.py

--- SONALI_MUSIC/plugins/bot/mustjoin.py
+++ SONALI_MUSIC/plugins/bot/mustjoin.py
@@ -5,7 +5,7 @@ from SONALI_MUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "PURVI_SUPPORT"
+MUST_JOIN = "BotsGrill"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
@@ -27,7 +27,7 @@ async def must_join_channel(app: Client, msg: Message):
                         [
                             [
                                 InlineKeyboardButton("• ᴊᴏɪɴ •", url=link),
-                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/KRITI_SUPPORT_CHAT"),
+                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/KomalMusicUpdates"),
                             ]
                         ]
                     )
